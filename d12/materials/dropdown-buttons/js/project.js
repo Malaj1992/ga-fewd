20a98841ea154184b5b6f20f20c9ea7ae871aa55
Malaj Angjelin <angjelin.malaj@lc.cuny.edu>
2017-04-04 15:57:34 -0400

    commit

---
+++ d12/materials/dropdown-buttons/js/project.js
@@ -0,0 +1,23 @@
+
+// $(document).ready(function(){
+//   $(".dropdown").on("hide.bs.dropdown", function(){
+//     $(".btn").html('a href="#">Open</a');
+//   });
+//   $(".dropdown").on("show.bs.dropdown", function(){
+//     $(".btn").html('a href="#">Open</a');
+//   });
+// });
+
+
+$(".dropdown").on("click", function() {
+	$(".dropdown-content").show();
+})
+
+// function myFunction() {
+//     var dropdown = document.getElementById('myDIV');
+//     if (dropdown.style.display === 'none') {
+//         dropdown.style.display = 'block';
+//     } else {
+//         dropdown.style.display = 'none';
+//     }
+// }
