d6c396f0e3088eea2e967cac17cfef34cde8c3b6
Malaj Angjelin <angjelin.malaj@lc.cuny.edu>
2017-04-11 19:31:03 -0400

    Commit

--- d15/materials/trip/js/app.js
+++ d15/materials/trip/js/app.js
@@ -4,6 +4,7 @@ var trip = [
     country: "UK",
     photo: "http://s3.evcdn.com/images/block/I0-001/024/126/410-9.jpeg_/waset-london-2017-conference-10.jpeg"
   },
+
   {
     city: "Budapest",
     country: "Hungary",
@@ -15,3 +16,26 @@ var trip = [
     photo: "http://s2.evcdn.com/images/block/I0-001/033/766/357-7.jpeg_/waset-lisbon-2017-conference-57.jpeg"
   }
 ];
+
+var changeTrip = function(indexYesSuper) {
+  $("h1").text(trip[indexYesSuper]["city"] + ", " + trip[indexYesSuper]["country"]);
+  $("img").attr("src", trip[indexYesSuper]["photo"]);
+}
+
+ $("#first").on("click", function() {
+// $("h1").text(trip[0]["city"] + "," + trip[0]["country"]);
+// $("img").attr("src", trip[0]["photo"]);
+   changeTrip(0);
+ });
+
+ $("#second").on("click", function() {
+// $("h1").text(trip[2]["city"] + "," + trip[1]["country"]);
+// $("img").attr("src", trip[1]["photo"]);
+changeTrip(1);
+ });
+
+ $("#third").on("click", function() {
+// $("h1").text(trip[2]["city"] + "," + trip[2]["country"]);
+// $("img").attr("src", trip[2]["photo"]);
+changeTrip(2);
+ });
